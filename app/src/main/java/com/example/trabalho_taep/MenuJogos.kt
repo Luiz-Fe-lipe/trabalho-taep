ca642eb8a45312d951ab78c441dbd172d42f9d75
Fernando <fernandoaraujocontato@outlook.com>
2024-08-29 19:39:49 -0300

    atualização

--- app/src/main/java/com/example/trabalho_taep/MenuJogos.kt
+++ app/src/main/java/com/example/trabalho_taep/MenuJogos.kt
@@ -1,6 +1,8 @@
 package com.example.trabalho_taep
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,19 @@ class MenuJogos : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu_jogos)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menujogos)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+        val imageButton = findViewById<ImageButton>(R.id.btn_jkp)
+        imageButton.setOnClickListener {
+            val intent = Intent(this, InicioJokenpo::class.java)
+            startActivity(intent)
+        }
+
     }
 }
